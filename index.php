db21d67979b576a4a6aaaf579ea837de55f8e1cc
Richard <richardcardozoof@gmail.com>
2025-10-23 08:44:47 -0500

    ActualizacionArchivos2

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>SENASOFT - Vuelos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/css/index.css">
-    <link rel="stylesheet" href="/css/coloresGblo.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/coloresGblo.css">
 </head>
 
 <body>
